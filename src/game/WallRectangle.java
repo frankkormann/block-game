fb4d9b0bd8f0ce6de441527de68e3bf74d75ced1
Frank Kormann <frankormann@outlook.com>
2025-07-01 13:33:58 -0400

    Fix y value

--- src/game/WallRectangle.java
+++ src/game/WallRectangle.java
@@ -23,7 +23,7 @@ public class WallRectangle extends Rectangle {
 			@JsonProperty("resizeBehavior") Rectangle.ResizeBehavior resizeBehavior) {
 		this(x, y, width, height, STAY_COLOR, resizeBehavior);
 
-		GroundingArea groundingArea = new GroundingArea(x, y, width, 1);
+		GroundingArea groundingArea = new GroundingArea(x, y - 1, width, 1);
 		getAttachments().add(groundingArea);
 	}
 
