c3f5f1640e521eb2adf708a37b5a2b01f6a0aac9
Frank Kormann <frankormann@outlook.com>
2025-07-01 13:25:37 -0400

    Create Area to tell MovingRectangles whether they are on ground or not

---
+++ src/game/GroundingArea.java
@@ -0,0 +1,25 @@
+package game;
+
+import java.awt.Color;
+
+public class GroundingArea extends Area {
+
+	public GroundingArea(int x, int y, int width, int height, Color color) {
+		super(x, y, width, height, new Color(0, 0, 0, 0));
+	}
+
+	@Override
+	public void onEnter(MovingRectangle rect) {
+		rect.setState(MovingRectangle.State.ON_GROUND);
+	}
+
+	@Override
+	public void onExit(MovingRectangle rect) {
+		rect.setState(MovingRectangle.State.IN_AIR);
+
+	}
+
+	@Override
+	public void everyFrame(MovingRectangle rect) {}
+
+}
