53e0ace757ed4d5f7d2fd466451f0d3ca7f3421a
Frank Kormann <frankormann@outlook.com>
2025-07-06 17:05:12 -0400

    Add test-friendly implementation of Area

---
+++ test/mocks/AccessibleArea.java
@@ -0,0 +1,42 @@
+package mocks;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+import game.Area;
+import game.MovingRectangle;
+import game.Rectangle;
+
+public class AccessibleArea extends Area {
+
+	public boolean hasEntered;
+	public boolean hasExited;
+	public Map<Rectangle, Integer> callsToEveryframe;
+
+	public AccessibleArea(int x, int y, int width, int height, Color color) {
+		super(x, y, width, height, color);
+		hasEntered = false;
+		hasExited = false;
+		callsToEveryframe = new HashMap<>();
+	}
+
+	@Override
+	public void onEnter(MovingRectangle rect) {
+		hasEntered = true;
+	}
+
+	@Override
+	public void onExit(MovingRectangle rect) {
+		hasExited = true;
+	}
+
+	@Override
+	public void everyFrame(MovingRectangle rect) {
+		if (!callsToEveryframe.containsKey(rect)) {
+			callsToEveryframe.put(rect, 0);
+		}
+		callsToEveryframe.put(rect, callsToEveryframe.get(rect) + 1);
+	}
+
+}
