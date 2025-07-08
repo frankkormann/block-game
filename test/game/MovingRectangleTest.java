726fe3e8c487c656b7063dbbf2f9d8664e704ef1
Frank Kormann <frankormann@outlook.com>
2025-07-08 19:55:40 -0400

    Move last... value tests into MovingRectangleTest
    These values are now stored in MovingRectangle

--- test/game/MovingRectangleTest.java
+++ test/game/MovingRectangleTest.java
@@ -1,11 +1,13 @@
 package game;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import game.MovingRectangle.State;
@@ -86,5 +88,97 @@ class MovingRectangleTest {
 
 		assertEquals(5, rect.getXVelocity());
 		assertEquals(0, rect.getYVelocity());
+	}
+
+	@Test
+	void adding_to_left_changes_leftWidthChange() {
+		rect.changeWidth(10, true);
+
+		assertEquals(10, rect.getLeftWidthChange());
+	}
+
+	@Test
+	void adding_to_right_does_not_change_leftWidthChange() {
+		rect.changeWidth(10, false);
+
+		assertEquals(0, rect.getLeftWidthChange());
+	}
+
+	@Test
+	void adding_to_top_changes_topHeightChange() {
+		rect.changeHeight(10, true);
+
+		assertEquals(10, rect.getTopHeightChange());
+	}
+
+	@Test
+	void adding_to_bottom_does_not_change_topHeightChange() {
+		rect.changeWidth(10, false);
+
+		assertEquals(0, rect.getTopHeightChange());
+	}
+
+	@Nested
+	class Intersections {
+
+		MovingRectangle other;
+
+		@BeforeEach
+		void createSecondRectangle() {
+			other = new MovingRectangle(rect.getX(), rect.getY(), rect.getWidth(),
+					rect.getHeight());
+		}
+
+		void updateLastPositions() {
+			rect.updateLastPosition();
+			other.updateLastPosition();
+		}
+
+		@Test
+		void used_to_intersect_x() {
+			other.setX(other.getX() - 5);
+			updateLastPositions();
+
+			assertTrue(rect.usedToIntersectX(other));
+			assertTrue(other.usedToIntersectX(rect));
+		}
+
+		@Test
+		void used_to_not_intersect_x() {
+			other.setX(other.getX() - 2 * other.getWidth());
+			updateLastPositions();
+
+			assertFalse(rect.usedToIntersectX(other));
+			assertFalse(other.usedToIntersectX(rect));
+		}
+
+		@Test
+		void used_to_intersect_y() {
+			other.setY(other.getY() - 5);
+			updateLastPositions();
+
+			assertTrue(rect.usedToIntersectY(other));
+			assertTrue(other.usedToIntersectY(rect));
+		}
+
+		@Test
+		void used_to_not_intersect_y() {
+			other.setY(other.getY() - 2 * other.getHeight());
+			updateLastPositions();
+
+			assertFalse(rect.usedToIntersectY(other));
+			assertFalse(other.usedToIntersectY(rect));
+		}
+
+		@Test
+		void completely_overlapping_used_to_intersect_both() {
+			updateLastPositions();
+
+			assertTrue(rect.usedToIntersectX(other));
+			assertTrue(other.usedToIntersectX(rect));
+
+			assertTrue(rect.usedToIntersectY(other));
+			assertTrue(other.usedToIntersectY(rect));
+		}
 	}
 }
