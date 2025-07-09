85c58e8cc0c082341a12b83244e8e3af13cf7ac5
Frank Kormann <frankormann@outlook.com>
2025-07-08 20:44:44 -0400

    Use DrawableMock instead of Rectangle

--- test/game/DrawingPaneTest.java
+++ test/game/DrawingPaneTest.java
@@ -10,19 +10,21 @@ import java.awt.image.BufferedImage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import mocks.DrawableMock;
+
 class DrawingPaneTest {
 
 	DrawingPane drawingPane;
 	BufferedImage bufferedImage;
-	Rectangle rect;
+	DrawableMock drawable;
 
 	@BeforeEach
 	void setUp() {
 		drawingPane = new DrawingPane();
 		bufferedImage = new BufferedImage(50, 50, BufferedImage.TYPE_INT_RGB);
-		rect = new WallRectangle(10, 10, 10, 10);
+		drawable = new DrawableMock(10, 10, 10, 10);
 
-		drawingPane.add(rect, 0);
+		drawingPane.add(drawable, 0);
 	}
 
 	@Test
@@ -54,32 +56,32 @@ class DrawingPaneTest {
 	void rectangle_is_drawn_at_x_y_when_offsets_are_zero() {
 		draw();
 
-		assertCornersNotColored(rect.getX(), rect.getY(), rect.getWidth(),
-				rect.getHeight());
+		assertCornersNotColored(drawable.x, drawable.y, drawable.width,
+				drawable.height);
 	}
 
 	@Test
-	void rectangle_is_drawn_offset_when_offsets_are_not_zero() {
+	void drawable_is_drawn_offset_when_offsets_are_not_zero() {
 		drawingPane.setXOffset(3);
 		drawingPane.setYOffset(2);
 		draw();
 
-		assertCornersNotColored(rect.getX() - 3, rect.getY() - 2, rect.getWidth(),
-				rect.getHeight());
+		assertCornersNotColored(drawable.x - 3, drawable.y - 2, drawable.width,
+				drawable.height);
 	}
 
 	@Test
-	void rectangles_which_overlap_are_drawn_in_correct_order() {
+	void drawables_which_overlap_are_drawn_in_correct_order() {
 		Rectangle other = new WallRectangle(15, 15, 10, 10);
 		drawingPane.add(other, 1);
 		draw();
 
-		int rectRGB = bufferedImage.getRGB(rect.getX(), rect.getY());
+		int rectRGB = bufferedImage.getRGB(drawable.x, drawable.y);
 		int otherRGB = bufferedImage.getRGB(other.getX() + other.getWidth() - 1,
 				other.getY() + other.getHeight() - 1);
 
-		assertNotEquals(rectRGB, bufferedImage.getRGB(rect.getX() + rect.getWidth() - 1,
-				rect.getY() + rect.getHeight() - 1));
+		assertNotEquals(rectRGB, bufferedImage.getRGB(drawable.x + drawable.width - 1,
+				drawable.y + drawable.height - 1));
 		assertEquals(otherRGB, bufferedImage.getRGB(other.getX(), other.getY()));
 	}
 }
