319409af98782e1c244403780a209df141d77fc1
Frank Kormann <frankormann@outlook.com>
2025-07-22 19:05:49 -0400

    Fix innaccuracy

--- src/game/MainFrame.java
+++ src/game/MainFrame.java
@@ -14,10 +14,9 @@ import javax.swing.UIManager;
  * JFrame that displays each level. Buffers changes to width/height between each
  * frame.
  * <p>
- * The usual methods for resizing and moving a JFrame should not be used for
- * this. The methods {@code resize}, {@code resizeAll}, and {@code move2} should
- * be used instead. {@code incorporateChanges} should be called at the end of
- * each frame.
+ * The usual methods for resizing a JFrame should not be used for this. The
+ * methods {@code resize} and {@code resizeAll} should be used instead.
+ * {@code incorporateChanges} should be called at the end of each frame.
  * <p>
  * Actual resizing of the window is delayed until {@code incorporateChanges} is
  * called at the end of each frame, after the new window size has been verified
