35da2b5265c9b38865c2ae5d5bc1734d0aeca8e4
Frank Kormann <frankormann@outlook.com>
2025-08-01 02:03:54 -0400

    Initial ParameterChangerPanel

---
+++ src/game/ParameterChangerPanel.java
@@ -0,0 +1,220 @@
+package game;
+
+import java.awt.Window;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeListener;
+
+import game.ParameterMapper.Parameter;
+
+/**
+ * 
+ */
+public class ParameterChangerPanel extends JPanel
+		implements ValueChangeListener {
+
+	private static final String UNDO_TEXT = "Undo changes";
+	private static final String RESET_TEXT = "Reset to defaults";
+
+	private static final int VERTICAL_SPACE = 3;
+
+	private ParameterMapper paramMapper;
+	private Map<Parameter, SliderSpinner> paramToSliderSpinner;
+
+	/**
+	 * 
+	 */
+	public ParameterChangerPanel(JRootPane rootPane, MainFrame mainFrame,
+			ParameterMapper paramMapper) {
+		super();
+		this.paramMapper = paramMapper;
+		paramToSliderSpinner = new HashMap<>();
+
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		SliderSpinner hintOpacity = new SliderSpinner(0f, 1f, 0f, 1f, 0.1f);
+		bindSliderSpinner(hintOpacity, Parameter.HINT_OPACITY);
+
+		add(createSliderSpinnerPanel(Parameter.HINT_OPACITY));
+		add(createUndoResetButtonPanel());
+
+		Window window = SwingUtilities.getWindowAncestor(rootPane);
+		window.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				cleanUp();
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				cleanUp();
+			}
+		});
+		paramMapper.addListener(this);
+	}
+
+	/**
+	 *
+	 */
+	private JPanel createUndoResetButtonPanel() {
+		JPanel panel = new JPanel();
+
+		JButton undoButton = new JButton(UNDO_TEXT);
+		undoButton.addActionListener(e -> {
+			paramMapper.reload();
+		});
+
+		JButton resetButton = new JButton(RESET_TEXT);
+		resetButton.addActionListener(e -> {
+			paramMapper.setToDefaults();
+		});
+
+		panel.add(undoButton);
+		panel.add(resetButton);
+
+		return panel;
+	}
+
+	/**
+	 * 
+	 * @param sliderSpinner
+	 * @param param
+	 */
+	private void bindSliderSpinner(SliderSpinner sliderSpinner,
+			Parameter param) {
+		sliderSpinner.setValue(paramMapper.get(param));
+		sliderSpinner.addChangeListener(e -> {
+			paramMapper.set(param, sliderSpinner.getValue());
+		});
+		paramToSliderSpinner.put(param, sliderSpinner);
+	}
+
+	/**
+	 * 
+	 * @param sliderSpinner
+	 * @param param
+	 * 
+	 * @return
+	 */
+	private JPanel createSliderSpinnerPanel(Parameter param) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+		JLabel label = new JLabel(paramToName(param));
+		label.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 0));
+
+		SliderSpinner sliderSpinner = paramToSliderSpinner.get(param);
+
+		panel.add(label);
+		panel.add(sliderSpinner.getSlider());
+		panel.add(sliderSpinner.getSpinner());
+
+		return panel;
+	}
+
+	/**
+	 * 
+	 * @param param
+	 * 
+	 * @return
+	 */
+	private String paramToName(Parameter param) {
+		return param.toString();
+	}
+
+	private void cleanUp() {
+		paramMapper.removeListener(this);
+		paramMapper.save();
+	}
+
+	@Override
+	public void valueChanged(Enum<?> key, Object newValue) {
+		if (paramToSliderSpinner.containsKey(key)) {
+			paramToSliderSpinner.get(key).setValue(paramMapper.get(key));
+		}
+	}
+
+	@Override
+	public void valueRemoved(Enum<?> key) {}
+
+	/**
+	 * 
+	 */
+	private class SliderSpinner {
+
+		private JSlider slider;
+		private JSpinner spinner;
+		private int sliderScaling;
+
+		public SliderSpinner(int sliderMin, int sliderMax, int spinnerMin,
+				int spinnerMax, int spinnerStep) {
+			sliderScaling = 1;
+			slider = new JSlider(sliderMin, sliderMax);
+			spinner = new JSpinner(new SpinnerNumberModel(0, spinnerMin,
+					spinnerMax, spinnerStep));
+
+			connectComponents();
+		}
+
+		public SliderSpinner(float sliderMin, float sliderMax, float spinnerMin,
+				float spinnerMax, float spinnerStep) {
+			sliderScaling = 100;
+			slider = new JSlider((int) (sliderMin * sliderScaling),
+					(int) (sliderMax * sliderScaling));
+			spinner = new JSpinner(new SpinnerNumberModel(0, spinnerMin,
+					spinnerMax, spinnerStep));
+
+			connectComponents();
+		}
+
+		private void connectComponents() {
+			slider.addChangeListener(e -> {
+				if (sliderScaling == 1) {
+					spinner.setValue(slider.getValue());
+				}
+				else {
+					spinner.setValue(slider.getValue() / (float) sliderScaling);
+				}
+			});
+			spinner.addChangeListener(e -> {
+				slider.setValue(
+						(int) (((Number) spinner.getValue()).doubleValue()
+								* sliderScaling));
+			});
+		}
+
+		public JSlider getSlider() {
+			return slider;
+		}
+
+		public JSpinner getSpinner() {
+			return spinner;
+		}
+
+		public void setValue(Number value) {
+			spinner.setValue(value);
+		}
+
+		public Number getValue() {
+			return (Number) spinner.getValue();
+		}
+
+		public void addChangeListener(ChangeListener listener) {
+			spinner.addChangeListener(listener);
+		}
+
+	}
+}
