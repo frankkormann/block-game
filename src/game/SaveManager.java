f156eff58b08b8612c42978a08fc6779c27ea4bb
Frank Kormann <frankormann@outlook.com>
2025-08-02 15:06:46 -0400

    Add support for putting arbitrary key/value pairs

--- src/game/SaveManager.java
+++ src/game/SaveManager.java
@@ -1,14 +1,14 @@
 package game;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.nio.file.Files;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Convenience class for writing and reading from a save file directory.
@@ -17,13 +17,16 @@ import java.nio.file.Files;
  * operating systems, it is in their home directory.
  * {@link #setDirectory(String)} should be called before any other methods to
  * configure the directory.
+ * <p>
+ * All file names should be prefixed with "/".
  */
 public class SaveManager {
 
 	private static final String DIRECTORY_NAME = "/BlockGame/save";
-	private static final String CURRENT_LEVEL_FILE = "/current_level";
+	private static final String SAVE_FILE_NAME = "/savedata";
 
 	private static String saveDirectory;
+	private static Map<String, String> cachedValues;
 	static {
 		if (System.getProperty("os.name").startsWith("Windows")) {
 			saveDirectory = System.getenv("APPDATA") + DIRECTORY_NAME;
@@ -97,62 +100,69 @@ public class SaveManager {
 	}
 
 	/**
-	 * Gets the most recently saved level resource.
+	 * Gets a value from the save file. If there is no saved value, returns
+	 * {@code defaultValue}.
 	 * 
-	 * @param defaultValue resource to use if there is no saved level or it is
-	 *                     not accessible
+	 * @param name         which value to get
+	 * @param defaultValue backup value in case there is no saved value
 	 * 
-	 * @return the most recently saved level
-	 * 
-	 * @see #setCurrentLevel(String)
+	 * @return the value
 	 */
-	public static String getCurrentLevel(String defaultValue) {
-		InputStream stream = readFile(CURRENT_LEVEL_FILE);
-		if (stream == null) {
-			return defaultValue;
+	public static String getValue(String name, String defaultValue) {
+		if (cachedValues == null) {
+			readValuesFromSave();
 		}
-		else {
+		return cachedValues.get(name) == null ? defaultValue
+				: cachedValues.get(name);
+	}
 
-			String returnValue;
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(stream));
-				returnValue = reader.readLine();
-				reader.close();
-				return returnValue;
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-				new ErrorDialog("Error", "Can't read save data", e)
-						.setVisible(true);
-				return defaultValue;
-			}
+	/**
+	 * Stores a value into the save data.
+	 * 
+	 * @param name  which value to store
+	 * @param value what to store
+	 */
+	public static void putValue(String name, String value) {
+		if (cachedValues == null) {
+			readValuesFromSave();
+		}
+		cachedValues.put(name, value);
+		saveToFile();
+	}
+
+	/**
+	 * Reads values from disk into {@code cachedValues}.
+	 */
+	private static void readValuesFromSave() {
+		InputStream stream = readFile(SAVE_FILE_NAME);
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			cachedValues = mapper.readValue(stream,
+					new TypeReference<Map<String, String>>() {});
+			stream.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			new ErrorDialog("Error", "Can't read save data", e)
+					.setVisible(true);
+			return;
 		}
 	}
 
 	/**
-	 * Saves {@code currentLevel} to disk.
-	 * 
-	 * @param currentLevel level resource to save
-	 * 
-	 * @see #getCurrentLevel(String)
+	 * Saves all values to disk.
 	 */
-	public static void setCurrentLevel(String currentLevel) {
-		BufferedWriter writer = new BufferedWriter(
-				new OutputStreamWriter(writeFile(CURRENT_LEVEL_FILE)));
+	private static void saveToFile() {
+		OutputStream outstream = writeFile(SAVE_FILE_NAME);
+		ObjectMapper mapper = new ObjectMapper();
 		try {
-			writer.write(currentLevel);
+			mapper.writeValue(outstream, cachedValues);
+			outstream.close();
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 			new ErrorDialog("Error", "Can't write save data", e)
 					.setVisible(true);
-		}
-		try {
-			writer.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
