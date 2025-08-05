cb3ca17ca58569fca2ba77d6fb908a94839b4b7d
Frank Kormann <frankormann@outlook.com>
2025-08-04 23:58:21 -0400

    Create supporting classes

---
+++ test/mocks/IntChangeListener.java
@@ -0,0 +1,30 @@
+package mocks;
+
+import game.Pair;
+import game.ValueChangeListener;
+
+public class IntChangeListener implements ValueChangeListener {
+
+	public String lastEvent;
+	public Pair<Enum<?>, Integer> eventInfo;
+
+	public IntChangeListener() {
+		lastEvent = "";
+		eventInfo = new Pair<>(null, null);
+	}
+
+	@Override
+	public void valueChanged(Enum<?> key, Object newValue) {
+		lastEvent = "valueChanged";
+		eventInfo.first = key;
+		eventInfo.second = (Integer) newValue;
+	}
+
+	@Override
+	public void valueRemoved(Enum<?> key) {
+		lastEvent = "valueRemoved";
+		eventInfo.first = key;
+		eventInfo.second = null;
+	}
+
+}
