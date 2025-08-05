af6aeb0944b51b419be22437f6ec0ea8ddcad4f3
Frank Kormann <frankormann@outlook.com>
2025-08-05 13:57:21 -0400

    Add tests for ValueChangerPanel

---
+++ test/mocks/ValueChangerPanelMock.java
@@ -0,0 +1,68 @@
+package mocks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+
+import game.GetterSetter;
+import game.Mapper;
+import game.ValueChangerPanel;
+
+public class ValueChangerPanelMock extends ValueChangerPanel<Integer> {
+
+	public Map<Enum<?>, GetterSetter<Integer>> getterSetters;
+
+	public ValueChangerPanelMock(JRootPane rootPane, Mapper<Integer> mapper) {
+		super(rootPane, mapper);
+	}
+
+	@Override
+	protected GetterSetter<Integer> createGetterSetter(Enum<?> enumValue) {
+		if (getterSetters == null) {
+			getterSetters = new HashMap<>();
+		}
+		GetterSetter<Integer> getterSetter = new IntGetterSetter();
+		getterSetters.put(enumValue, getterSetter);
+		return getterSetter;
+	}
+
+	@Override
+	protected JPanel createGetterSetterPanel(
+			Map<Enum<?>, GetterSetter<Integer>> getterSetters) {
+		return new JPanel();  // TODO Figure out a way to test GUI elements
+	}
+
+	private class IntGetterSetter implements GetterSetter<Integer> {
+
+		int value;
+		Runnable changeListener;
+
+		public IntGetterSetter() {
+			value = 0;
+			changeListener = () -> {};
+		}
+
+		@Override
+		public Integer get() {
+			return value;
+		}
+
+		@Override
+		public void set(Integer value) {
+			boolean valueChanged = value != this.value;
+			this.value = value;
+			if (valueChanged) {
+				changeListener.run();
+			}
+		}
+
+		@Override
+		public void addChangeListener(Runnable runnable) {
+			changeListener = runnable;
+		}
+
+	}
+
+}
