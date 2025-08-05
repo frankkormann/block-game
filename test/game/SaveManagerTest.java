d379b132449a2c67721d1184d012fccf93ef4ec6
Frank Kormann <frankormann@outlook.com>
2025-08-05 13:24:40 -0400

    Add tests for SaveManager

---
+++ test/game/SaveManagerTest.java
@@ -0,0 +1,48 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class SaveManagerTest {
+
+	@BeforeEach
+	void setUp(@TempDir Path dir) {
+		SaveManager.setDirectory(dir.toString());
+	}
+
+	@Test
+	void can_write_a_new_file_and_read_it() throws IOException {
+		OutputStream out = SaveManager.writeFile("/test");
+		byte[] content = new byte[] { 1, 2, 3, 4, 5 };
+		out.write(content);
+		out.close();
+
+		InputStream in = SaveManager.readFile("/test");
+		for (byte b : content) {
+			assertEquals(b, in.read());
+		}
+		in.close();
+	}
+
+	@Test
+	void can_put_a_value_and_get_it() {
+		String name = "foo";
+		String value = "bar";
+		SaveManager.putValue(name, value);
+
+		assertEquals(value, SaveManager.getValue(name, ""));
+	}
+
+	@Test
+	void returns_default_value_if_value_doesnt_exist() {
+		assertEquals("potato", SaveManager.getValue("I'm not here", "potato"));
+	}
+}
