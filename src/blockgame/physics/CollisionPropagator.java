5660727d3f4d9c48fe33303f98f7d55d0246a9f7
Frank Kormann <frankormann@outlook.com>
2025-08-05 17:06:04 -0400

    Use global colliders

--- src/blockgame/physics/CollisionPropagator.java
+++ src/blockgame/physics/CollisionPropagator.java
@@ -74,7 +74,7 @@ public class CollisionPropagator {
 			throw new IllegalStateException("Already propagated collision");
 		}
 		completed = true;
-		return propagateCollision(initialRect, colliders, new HashMap<>());
+		return propagateCollision(initialRect, new HashMap<>());
 	}
 
 	/*
@@ -87,19 +87,18 @@ public class CollisionPropagator {
 	 * and how much
 	 */
 	private int[] propagateCollision(MovingRectangle rect,
-			Collection<MovingRectangle> colliders,
 			Map<MovingRectangle, Pair<MovingRectangle, int[]>> collisionMap) {
 
 		int[] collisionData;
 		int[] pushedAmount = { 0, 0 };
-		colliders = new ArrayList<>(colliders);
 		colliders.remove(rect);
 
 		int[] wallPushback = handleCollisionWithWalls(rect);
 		pushedAmount[0] += wallPushback[0];
 		pushedAmount[1] += wallPushback[1];
 
-		for (MovingRectangle other : colliders) {
+		for (MovingRectangle other : new ArrayList<MovingRectangle>(
+				colliders)) {
 
 			collisionData = calculateCollision(rect, other);
 			if (collisionData[0] == 0 && collisionData[1] == 0) {
@@ -129,7 +128,7 @@ public class CollisionPropagator {
 			collisionMap.put(other,
 					new Pair<MovingRectangle, int[]>(rect, collisionData));
 
-			int[] pushback = propagateCollision(other, colliders, collisionMap);
+			int[] pushback = propagateCollision(other, collisionMap);
 
 			if (collisionData[0] != 0) {  // rect should only be pushed back in
 										  // the direction it pushed other
