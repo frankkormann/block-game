15fc00052f30a9bff27d5a1b5ab7d70a4e5630b6
Frank Kormann <frankormann@outlook.com>
2025-08-21 22:22:24 -0400

    Add VolumeChangerPanel

---
+++ src/blockgame/input/VolumeChangerPanel.java
@@ -0,0 +1,54 @@
+package blockgame.input;
+
+import javax.swing.JRootPane;
+
+import blockgame.input.VolumeMapper.Volume;
+
+/**
+ * {@code NumberChangerPanel} which allows the user to remap values in
+ * {@code VolumeMapper}. Automatically calls {@code Mapper.save} the parent
+ * window is closed.
+ * 
+ * @author Frank Kormann
+ */
+public class VolumeChangerPanel extends NumberChangerPanel {
+
+	/**
+	 * Creates a {@code VolumeChangerPanel} which will alter
+	 * {@code volumeMapper}.
+	 * 
+	 * @param rootPane     {@code JRootPane} of parent window
+	 * @param volumeMapper {@code VolumeMapper} to alter
+	 */
+	public VolumeChangerPanel(JRootPane rootPane, VolumeMapper volumeMapper) {
+		super(rootPane, volumeMapper);
+	}
+
+	@Override
+	protected SliderSpinner createSliderSpinner(Enum<?> enumValue) {
+		return new SliderSpinner(0, 200, 0, 200, 5, true, "%");
+	}
+
+	@Override
+	protected String paramToName(Enum<?> enumValue) {
+		if (enumValue == Volume.SFX) {
+			return "Sound Effects Volume";
+		}
+		if (enumValue == Volume.MUSIC) {
+			return "Music Volume";
+		}
+
+		return enumValue.toString();
+	}
+
+	@Override
+	protected String paramToTooltip(Enum<?> enumValue) {
+		return null;
+	}
+
+	@Override
+	protected Enum[] getEnumValues() {
+		return Volume.values();
+	}
+
+}
